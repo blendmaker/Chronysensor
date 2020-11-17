cc0f30a760b6ec61e31df9f192f0aee85fd445eb
Klaus Daume <klausdaume@gmail.com>
2020-11-17 17:13:56 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+
+const uint8_t outputPins[] = { 18, 19, 20, 21 };
+const uint8_t input1 = 2;
+const uint8_t input2 = 3;
+
+const float distance = 100 * 0.001f; // in m
+
+volatile unsigned long startTime = 0;
+volatile double travelTime = 0;
+
+void inputTriggered() {
+  startTime = micros();
+}
+
+void outputTriggered() {
+  travelTime = (micros()-startTime)*0.000001f;
+  startTime = 0;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  for (byte i = 0; i <  (sizeof(outputPins) / sizeof(outputPins[0])); i++) {
+    pinMode( outputPins[i], OUTPUT );
+    digitalWrite( outputPins[i], HIGH );
+  }
+
+  pinMode(input1, INPUT_PULLUP);
+  pinMode(input2, INPUT_PULLUP);
+
+  
+  attachInterrupt( digitalPinToInterrupt(input1), inputTriggered,  FALLING);
+  attachInterrupt( digitalPinToInterrupt(input2), outputTriggered,  FALLING);
+}
+
+void loop() {
+  if (travelTime > 0) {
+    double mps = distance / travelTime;
+    Serial.println(String(mps));
+    travelTime = 0;
+  }
+}
